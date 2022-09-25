3bed9c1a1f52f82f3644a692c2e303716895a362
Petr Koller <kollerpetr00@gmail.com>
2022-09-25 16:51:16 +0200

    Testing repaired

--- data-access/db-connection.js
+++ data-access/db-connection.js
@@ -2,26 +2,10 @@ const mongoose = require("mongoose");
 const { AppError, errorHandler } = require("../libraries/error-handling");
 const { logger } = require("../libraries/logger");
 
-function registerErrorListeners() {
-    mongoose.connection.on("error", (err) => {
-        errorHandler.handleError(
-            new AppError("mongodb-error", "MongoDB emitted error", err, 500, false)
-        );
-    });
-
-    // TODO Poresit, pri skonceni testu killnu mongo, coz zavola tenhle event a dojde k vyvolani erroru, ktery hazi process.exit called with ""
-    mongoose.connection.on("disconnected", (err) => {
-        errorHandler.handleError(
-            new AppError("mogodb-disconnected-error", "Lost connection to MongoDB", err, 500, false)
-        );
-    });
-}
-
 async function connectToDB(connectionString) {
     try {
         logger.info("Connecting to DB");
         await mongoose.connect(connectionString);
-        registerErrorListeners();
         logger.info("Successfully connected to DB");
     } catch (err) {
         errorHandler.handleError(
